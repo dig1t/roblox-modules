8df6b4539d519eb30ba8d8a978c7b28e948a1b7c
dig1t <admin@javierm.net>
2022-08-12 03:38:37 -0500

    support ReplicatedStorage imports

--- src/init.lua
+++ src/init.lua
@@ -1,15 +1,21 @@
 --[[
-@name dLib
+@name dLib Importer
 @author dig1t
 ]]
 
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
 local Util = require(script.Util)
+
+local PATH_DIVIDER = '/'
+
+local exports = {}
 
 local loadedModules = {
 	Util = Util -- We already required the Util module
 }
 
-local function import(path)
+function exports.import(path)
 	assert(typeof(path) == 'string', 'dLib.import - Path is not a string')
 
 	-- Return module if it was already used
@@ -17,29 +23,29 @@ local function import(path)
 		return loadedModules[path]
 	end
 
-	local modulePath = Util.treePath(script, path, '/')
+	local modulePath = Util.treePath(script, path, PATH_DIVIDER)
+	
+	-- Tree search through ReplicatedStorage if there is no dLib module found
+	if not modulePath then
+		modulePath = Util.treePath(ReplicatedStorage, path, PATH_DIVIDER)
+	end
 
 	assert(modulePath, 'dLib.import - Missing module ' .. path)
-	--[[assert(
+	assert(
 		modulePath:IsA('ModuleScript'),
 		string.format('dLib.import - %s is not a ModuleScript instance', path)
-	)]]
-
-	if modulePath and modulePath:IsA('ModuleScript') then
-		local success, res = pcall(function()
-			return require(modulePath)
-		end)
-
+	)
+	
+	local success, res = pcall(function()
+		return require(modulePath)
+	end)
 		if not success then
-			error('dLib.import - ' .. res)
-		end
-
-		loadedModules[path] = res
-
-		return res
+		error('dLib.import - ' .. res)
 	end
+	
+	loadedModules[path] = res
+	
+	return res
 end
 
-return {
-	import = import
-}
+return exports
