780203a56af425a8ce3c95608f9eb9984f202cdb
dig1t <admin@javierm.net>
2022-09-18 00:25:11 -0500

    maid

--- src/Maid.lua
+++ src/Maid.lua
@@ -1,32 +1,73 @@
-local Maid, methods = {}, {}
-methods.__index = methods
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
 
-function methods:addTask(task)
+local import = require(ReplicatedStorage.Packages.dLib).import
+local Util = import('Util')
+
+local Maid = {}
+Maid.__index = Maid
+
+function Maid.hire(): table
+	local self = setmetatable({}, Maid)
 	
+	self._tasks = {}
+	
+	return self
 end
 
-function methods:clean()
-	for id, task in pairs(self._tasks) do
-		if typeof(task) == 'function' then
-			task()
-		elseif typeof(task) == 'RBXScriptConnection' then
+function Maid:assign(task: any, destructor: any?): string
+	local taskId = Util.randomString(14)
+	
+	self._tasks[taskId] = destructor and {
+		task = task,
+		destructor = destructor
+	} or task
+	
+	return taskId
+end
+
+function Maid:removeTask(taskToRemove: any): nil
+	for taskId, task in pairs(self._tasks) do
+		if task == taskToRemove then
+			self._tasks[taskId] = nil
+		end
+	end
+end
+
+function Maid:cleanTasks(taskIds: table?): nil
+	local tasks = taskIds and {} or self._tasks
+	
+	if taskIds then
+		for _, taskId in pairs(taskIds) do
+			if self._tasks[taskId] then
+				tasks[taskId] = self._tasks[taskId]
+			end
+		end
+	end
+	
+	for taskId, _ in pairs(tasks) do
+		local taskType = typeof(task)
+		
+		if taskType == 'function' then
+			task() -- Run cleaning task
+		elseif taskType == 'RBXScriptConnection' and task.Connected then
 			task:Disconnect()
 		elseif task.Destroy then
 			task:Destroy()
 		elseif task.destroy then -- UIKit object
 			task:destroy()
+		elseif taskType == 'table' then
+			if task.destructor then
+				task.destructor(task.task or task)
+			end
 		end
 		
-		self.tasks[id] = nil
+		self._tasks[taskId] = nil
 	end
 end
 
-function Maid.new()
-	local self = setmetatable({}, methods)
-	
-	self._tasks = {}
-	
-	return self
+function Maid:clean(): nil
+	self:cleanTasks()
+	self._tasks = nil
 end
 
 return Maid
